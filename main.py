b8c08e66c69d9d3bfe9516ecf65be08429e899cd
msansari3060 <47550489+msansari3060@users.noreply.github.com>
2025-07-13 22:32:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+
 from flask import Flask, request, send_file
 from rembg import remove
 from io import BytesIO
 from PIL import Image
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains
 
 @app.route('/')
 def home():
@@ -29,7 +32,5 @@ def remove_watermark():
     return send_file(output_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get("PORT", 5000))
-    print(f"🚀 Starting Flask server on port {port}")
     app.run(host='0.0.0.0', port=port)
